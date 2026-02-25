23215cb7d083e9002232ba5106d5e0f917d5b722
Vinicius Kenji dos Santos Enoki <viniciuskenji@PREFEITURA.SP.GOV.BR>
2026-02-25 11:57:43 -0300

    fix: removendo estilos setados

--- interfaces/pages/home/aboutSection/index.tsx
+++ interfaces/pages/home/aboutSection/index.tsx
@@ -3,7 +3,7 @@
 
 export const AboutSection = () => {
     return (
-        <div className="h-dvh w-full bg-black flex items-center justify-center">
+        <div className="h-dvh w-full bg-black flex items-start justify-start p-60">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-4xl font-bold text-white">About Me</h1>
                 <p className="text-white mt-6 max-w-2xl text-center">
