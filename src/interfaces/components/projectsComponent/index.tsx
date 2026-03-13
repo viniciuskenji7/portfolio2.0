5c9879de995837f440c6bf8e46956616aba79ea1
Vinicius Kenji dos Santos Enoki <viniciuskenji@PREFEITURA.SP.GOV.BR>
2026-03-13 17:12:49 -0300

    styles: alterando hover projectComponent

--- src/interfaces/components/projectsComponent/index.tsx
+++ src/interfaces/components/projectsComponent/index.tsx
@@ -15,31 +15,25 @@ export const ProjectsComponent = ({ projectImage, projectTitle, projectTecnologi
     const iconVerify = projectTecnologies;
 
     return (
-        <div className="h-full w-200">
-            <Link
-                href={'/'}
-                target="_blank"
-                className="hover:transform hover:scale-102 transition-all"
-            >
-                <div className="p-5 border border-white rounded-md w-full h-full bg-white flex flex-col items-start justify-center gap-5 text-black">
-                    <h3 className="text-2xl font-[poppins]">{projectTitle}</h3>
-                    <Image
-                        alt="Image Projects"
-                        src={projectImage.src}
-                        height={projectImage.height}
-                        width={projectImage.width}
-                        className="h-full w-full rounded-md"
-                    />
-                    <div className="flex justify-start items-center gap-2">
-                        {icons
-                            .filter(item => iconVerify.some(tech => item.title.includes(tech)))
-                            .map((item, index) => (
-                                <item.Icon key={index} className="w-15 h-15" />
-                            ))
-                        }
-                    </div>
+        <div className="h-full w-200 hover:transform hover:scale-102 transition-all cursor-pointer">
+            <div className="p-5 border border-white rounded-md w-full h-full bg-white flex flex-col items-start justify-center gap-5 text-black">
+                <h3 className="text-2xl font-[poppins]">{projectTitle}</h3>
+                <Image
+                    alt="Image Projects"
+                    src={projectImage.src}
+                    height={projectImage.height}
+                    width={projectImage.width}
+                    className="h-full w-full rounded-md"
+                />
+                <div className="flex justify-start items-center gap-2">
+                    {icons
+                        .filter(item => iconVerify.some(tech => item.title.includes(tech)))
+                        .map((item, index) => (
+                            <item.Icon key={index} className="w-15 h-15" />
+                        ))
+                    }
                 </div>
-            </Link>
+            </div>
         </div>
     );
 }
