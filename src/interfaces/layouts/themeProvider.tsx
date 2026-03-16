962e2b3ecacd2f47d8672873e6855b7b1dbd0ed0
viniciuskenji7 <viniciuskenji007@gmail.com>
2026-03-16 14:07:43 -0300

    refactor: navbar funcional

--- src/interfaces/layouts/themeProvider.tsx
+++ src/interfaces/layouts/themeProvider.tsx
@@ -10,10 +10,10 @@ interface ThemeProviderProps {
 export const ThemeProvider = ({children}: ThemeProviderProps) => {
     
     const menuItems = [
-        { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
-        { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
-        { label: 'Services', ariaLabel: 'View our services', link: '/services' },
-        { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
+        { label: 'Home', ariaLabel: 'Go to home page', link: '#home' },
+        { label: 'About', ariaLabel: 'Learn about me', link: '#about' },
+        { label: 'Projects', ariaLabel: 'View my projects', link: '#projects' },
+        { label: 'Contact', ariaLabel: 'Get in touch', link: '#contact' }
     ];
     
     const socialItems = [
