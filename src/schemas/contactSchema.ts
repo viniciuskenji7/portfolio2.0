8597f58ee1a2f84a78e29964c980f12760fc480e
Vinicius Kenji dos Santos Enoki <viniciuskenji@PREFEITURA.SP.GOV.BR>
2026-03-18 12:58:06 -0300

    feat: configurando mensagens de erro do formContact

--- src/schemas/contactSchema.ts
+++ src/schemas/contactSchema.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const Email = z.object({
-    name: z.string().min(2),
-    email: z.email(),
-    message: z.string().min(5),
+    name: z.string(),
+    email: z.email("Email Inválido"),
+    message: z.string().min(5, "Mensagem muito curta"),
 });
 
